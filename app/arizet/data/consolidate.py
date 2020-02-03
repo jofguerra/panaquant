2bc02905c4524f498e88f214ff8d8eb3d3d0d9d9
juanccs <juansaenz.work@gmail.com>
2020-02-03 01:15:19 -0500

    Improved consolidator to include signals, works well in Quantopian

--- app/arizet/data/consolidate.py
+++ app/arizet/data/consolidate.py
@@ -26,18 +26,22 @@ def read_csv_files(remove_heading=False):
             df_filtered = df_filtered.loc[df_filtered['Volume'] >= 1e5]
             df_filtered = df_filtered.loc[df_filtered['Trades'] >= 3]
             df_filtered = df_filtered.loc[df_filtered['frequency'] > 3]
+            # df_filtered = df_filtered.loc[df_filtered['Signal'] == 'Buy']
             date_string = filename.split('.')[0][-8:]
             date = dt.datetime.strptime(date_string, '%Y%m%d')
-            stocks = set(df_filtered['Ticker'])
-            date_df = pd.DataFrame()
-            date_df['ticker'] = list(stocks)
-            date_df['date'] = date
+            stocks = set(zip(df_filtered['Ticker'], df_filtered['Signal']))
+            # TODO: Remove tickers that appear in both sell and buy
+            rows = []
+            for stock in stocks:
+                rows.append({'ticker': stock[0], 'signal': stock[1], 'date': date})
+            date_df = pd.DataFrame(rows)
             out_df = pd.concat([out_df, date_df])
 
     out_df = out_df.sort_values(by='date')
     out_df = out_df.reset_index(drop=True)
+    out_df.index.name = 'index'
     pprint(out_df)
-    out_df.to_csv(os.path.join(dir_path, 'out_df.csv'))
+    out_df.to_csv(os.path.join(dir_path, 'out_df_97.csv'))
 
 
 if __name__ == "__main__":
